8ccde6b8c6c4ac39ef668ccda08c2c8e76744b80
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-10-29 11:32:18 -0700

    starting setup

---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+function ManageExpense() {
+    return (
+        <Text>
+            
+        </Text>
+    )
+}
+
+export default ManageExpense
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+function RecentExpenses() {
+    return (
+        <Text></Text>
+    )
+}
+
+export default RecentExpenses;
